1577b384758cb1538d20f6d890e89d965c640edc
Daniel Ma <52453932+danielma911@users.noreply.github.com>
2025-08-13 16:15:52 +0800

    Create gcs2.tf

---
+++ code/build/gcs2.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 5.0"
+    }
+  }
+}
+
+
+provider "google" {
+  project = "qwiklabs-gcp-03-ef77dfc22296"
+  region  = "us-central1"
+}
+
+resource "google_storage_bucket" "example" {
+  name          = "demo-${random_id.rand_suffix.hex}"
+  location      = "us-central1"
+  force_destroy = yes
+
+  uniform_bucket_level_access = false
+
+}
+
+resource "random_id" "rand_suffix" {
+  byte_length = 4
+}
+
+output "bucket_name" {
+  value = google_storage_bucket.example.name
+}
