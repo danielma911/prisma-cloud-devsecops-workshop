a4bf546463bb3d8c7a064a35619f161635ce4984
Daniel Ma <18621833570@163.com>
2025-08-20 11:24:43 +0800

    up3

--- code/deployment_gcs_bucket.tf
+++ code/deployment_gcs_bucket.tf
@@ -1,5 +1,11 @@
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
+
+
 resource "google_storage_bucket" "example" {
-  name          = "demo-${random_id.Rand_suffix.hex}"
+  name          = "demo-${random_id.rand_suffix.hex}"
   location      = "us-central1"
   force_destroy = true
 
